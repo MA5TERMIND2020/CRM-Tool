c5f6599df363f936a0cce4826e68a76b45236eb4
Puffyfish <93864592+Puffyfish@users.noreply.github.com>
2022-08-21 08:45:07 +0200

    Sales/new page created
    - created routes sales/new
    - added color for input

--- routes/sale.js
+++ routes/sale.js
@@ -9,4 +9,10 @@ router.get('/', async (req, res) => {
   res.render('pages/sales/index');
 })
 
+router.get('/new', async(req, res) => {
+  const customers = await Customer.find({});
+  const products = await Product.find({});
+  res.render('pages/sales/new', {customers, products})
+})
+
 module.exports = router;
